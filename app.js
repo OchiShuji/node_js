296e473823ec8a5ce3eaa2e95e24bd8344158334
Ochi Shuji <ochimms@ybb.ne.jp>
2022-10-09 16:07:14 +0900

    Lesson I completed.

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+port = 3000;
 
+app.use(express.static('public'));
+
+/*
 app.get('/', (req,res) => {
     res.render('hello.ejs');
 });
+*/
 
-app.listen(12000);
+app.get('/', (req,res) => {
+    res.render('top.ejs');
+});
+
+app.get('/index', (req,res) => {
+    res.render('index.ejs');
+});
+
+app.listen(port, () => {
+    console.log('listen: ' + port);
+});
